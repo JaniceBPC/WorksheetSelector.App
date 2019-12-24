5d4724a885226bb376d0436f7c29659cd7c829d8
JaniceBPC <janice.kierstead@gmail.com>
2019-12-24 05:49:15 -0500

    Local

--- WorksheetSelectorApp/App.xaml.cs
+++ WorksheetSelectorApp/App.xaml.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ReactiveUI;
+using Splat;
+using System.Reflection;
 using System.Windows;
 
 namespace WorksheetSelectorApp
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+
+        //Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetCallingAssembly());
     }
 }
